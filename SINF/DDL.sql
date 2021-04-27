91cdfacfd7642634dbbd8bfa5ec44fcdb7bcc77c
dany3cunha <up201806651@fe.up.pt>
2021-04-27 22:44:58 +0100

    Update DDL.sql

--- SINF/DDL.sql
+++ SINF/DDL.sql
@@ -15,20 +15,24 @@ CREATE TABLE actuator (
 
 CREATE TABLE actuator_vec (
 
-    date TIMESTAMP,
+    date TIMESTAMP ,
+    name_act VARCHAR(45) NOT NULL,
     state BOOLEAN NOT NULL,
-    CONSTRAINT PK_act_vec PRIMARY KEY (date,name)
+    CONSTRAINT PK_act_vec PRIMARY KEY (date,name_act)
 );
 
 CREATE TABLE op_r_subr (
 
+    subrule_id INT,
+    rule_id INT,
     op_between_rules VARCHAR(5),
-    CONSTRAINT PRIMARY KEY (subrule_id) 
+    CONSTRAINT PK_op_r_subr PRIMARY KEY (subrule_id) 
 );
 
 CREATE TABLE subrule(
 
     subrule_id SERIAL,
+    sensor_name VARCHAR(45) NOT NULL,
     operation VARCHAR(2) NOT NULL,
     ref NUMERIC(3,2) NOT NULL,
     CONSTRAINT PK_subRule PRIMARY KEY (subrule_id)
@@ -38,6 +42,7 @@ CREATE TABLE subrule(
 CREATE TABLE sensor (
 
     name VARCHAR(45),
+    mote_id INT NOT NULL,
     actual_value NUMERIC(3,2) NOT NULL ,
     CONSTRAINT PK_sens_name PRIMARY KEY (name)
 );
@@ -46,12 +51,14 @@ CREATE TABLE sensor_vec (
     
     date TIMESTAMP,
     value NUMERIC(3,2) NOT NULL ,
-    CONSTRAINT PK_sens_vec PRIMARY KEY (date,name)
+    name_sens VARCHAR(45),
+    CONSTRAINT PK_sens_vec PRIMARY KEY (date,name_sens)
 );
 
 CREATE TABLE mote (
 
     mote_id INT,
+    section_id INT NOT NULL,
     CONSTRAINT PK_mote PRIMARY KEY (mote_id)
 );
 
@@ -64,25 +71,25 @@ ALTER TABLE rule ADD CONSTRAINT FK_RuleActName
     FOREIGN KEY (name) REFERENCES actuator (name) NOT NULL;
 
 ALTER TABLE actuator_vec ADD CONSTRAINT PK_Name
-    FOREIGN KEY (name) REFERENCES actuator(name);
+    FOREIGN KEY (name_act) REFERENCES actuator(name);
 
 ALTER TABLE op_r_subr CONSTRAINT PK_subruleID
-    FOREIGN KEY (subrule_id) REFERENCES (subrule);
+    FOREIGN KEY (subrule_id) REFERENCES subrule(subrule_id);
 
 ALTER TABLE op_r_subr CONSTRAINT FK_rule_id 
-    FOREIGN KEY (rule_id) REFERENCES (rule) NOT NULL;
+    FOREIGN KEY (rule_id) REFERENCES rule(rule_id) NOT NULL;
 
 ALTER TABLE subrule CONSTRAINT FK_sensorName
-    FOREIGN KEY (name) REFERENCES (sensor) NOT NULL;
+    FOREIGN KEY (sensor_name) REFERENCES sensor(name) NOT NULL;
 
 ALTER TABLE sensor CONSTRAINT FK_moteID
-    FOREIGN KEY (mote_id) REFERENCES (mote) NOT NULL;
+    FOREIGN KEY (mote_id) REFERENCES mote(mote_id) NOT NULL;
 
 ALTER TABLE sensor_vec CONSTRAINT PK_Name 
-    FOREIGN KEY (name) REFERENCES (sensor) NOT NULL;
+    FOREIGN KEY (name_sens) REFERENCES sensor(name) NOT NULL;
 
 ALTER TABLE mote_id CONSTRAINT FK_sectionID
-    FOREIGN KEY (section_id) REFERENCES (section) NOT NULL;
+    FOREIGN KEY (section_id) REFERENCES section(section_id) NOT NULL;
 
 
 
